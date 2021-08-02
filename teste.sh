4675704f674eefb569e301aa8d16dcf53da47526
Tony Anderson <40336088+TonyAnderson98@users.noreply.github.com>
2021-08-02 11:19:22 -0300

    Dependências
    Melhorias:
    - As dependências agora são gerenciadas por flags
    -  Os status das dependências agora são visuais (coloridos)
    - Correção de bugs e pequenas melhorias

--- teste.sh
+++ teste.sh
@@ -1,36 +1,34 @@
+verde="\033[1;32m"
+vermelho="\033[1;31m"
+limpar="\033[0m"
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-zenity --question \
---title="Gfortran" \
---text "Deseja instalar o gfortran agora?" \
---ok-label="Sim" \
---cancel-label="Não"
-
-
-
-if [ $? = 0 ]; then
-    sudo apt-get install gfortran
+if ! command -v xmakemol &> /dev/null; then
+    printf "[${vermelho}Erro${limpar}] xmakemol não está instalado \n"
+    DEP_XMAKEMOL=FALSE                      # Muda a flag para indicar que a dependência xmakemol não está satisfeita
 else
-    echo "Ok. Não insalaremos o gfortran, mas não será possível continuar. Saíndo..."
+	printf "[ ${verde}OK${limpar} ] xmakemol encontrado em "
+	command -v gfortran
 fi
+
+echo "--------------------------"
+
+
+if [ $DEP_XMAKEMOL = FALSE ]; then
+    zenity --question  \
+    --title="xmakemol" \
+    --text "Deseja instalar o xmakemol agora?" \
+    --ok-label="Sim" \
+    --cancel-label="Não"
+
+    if [ $? = 0 ]; then
+        sudo apt-get install xmakemol
+    else
+        echo "Ok. Não insalaremos o xmakemol, mas não será possível continuar. Saíndo..."
+        exit 1
+    fi
+fi
+# =================================================================================
